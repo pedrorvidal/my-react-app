407209e698513c8490fc2d1c7bb6935560d4019f
Pedro Vidal <pedrorvidal@gmail.com>
2025-05-12 23:40:10 -0300

    hearts for likes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  });
 
   return (
     <div className="card">
       <h2>{title}</h2>
       <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
+        {hasLiked ? '❤️' : '🤍'}
       </button>
     </div>
   );
 };
 
 const App = () => {
-
   return (
     <div className="card-container">
       <Card title="Star Wars" />
