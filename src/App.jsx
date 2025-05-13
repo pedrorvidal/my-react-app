dce395b2511fb294b40fe6dd8138659f66de49c4
Pedro Vidal <pedrorvidal@gmail.com>
2025-05-13 00:10:26 -0300

    more useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react';
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
 
   useEffect(() => {
     console.log(`${title} has been liked: ${hasLiked}`);
-  });
+  }, [hasLiked]);
+  useEffect(() => {
+    console.log('card rendered');
+  }, []);
 
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h3>
+        {title} <br /> {count || null}
+      </h3>
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '❤️' : '🤍'}
       </button>
