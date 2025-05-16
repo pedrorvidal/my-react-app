1f1a8ffcf3d7b2f8c66b7ceb5a9da131a1bad7c5
Pedro Vidal <pedrorvidal@gmail.com>
2025-05-15 23:39:28 -0300

    starting header design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">movies</span> You'll enjoy Without the Hassle</h1>
+        </header>
+        <p>Search</p>
+      </div>
+
+    </main>
   );
 };
 
